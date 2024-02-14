3fae326171d4daa029495c3513b1cc0a102dd5a7
biclimp <112048831+biclimp@users.noreply.github.com>
2024-02-14 12:33:16 +0800

    Update prosuser.php

--- prosuser.php
+++ prosuser.php
@@ -1,12 +1,14 @@
-<?php include("link.php");
-
-$nama = $_POST['name'];
+<?php include("config.php"); //tukar link.php kepada config.php
+    
+$name = $_POST['name']; //tukar $nama kepada $name
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = INSERT INTO user VALUES ('','$name','$username','$email','$pssword','2')";
-$result = mysql_query($sql);
+$sql = "INSERT INTO user VALUES ('','$name','$username','$email','$password','2')"; 
+$result = mysqli_query($conn, $sql); //tukar mysql_query kepada mysqli_query //tambah $conn
 
-
+header("Location: index.php"); //tiada directory ke laman utama
 ?>
+
+
